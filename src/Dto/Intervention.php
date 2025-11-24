ef91d5227f8049a37fd21abb6bae73f523d23caf
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-11-24 13:05:48 +0200

    feat: Create Intervention DTO

---
+++ src/Dto/Intervention.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ChernegaSergiy\Nk0262021Parser\Dto;
+
+class Intervention
+{
+    public function __construct(
+        public readonly string $class_code,
+        public readonly string $class_name,
+        public readonly int $anatomical_site_code,
+        public readonly string $anatomical_site_name,
+        public readonly int $procedure_type_code,
+        public readonly string $procedure_type_name,
+        public readonly int $procedure_group_code,
+        public readonly string $procedure_group_name,
+        public readonly string $code,
+        public readonly string $name_ua,
+        public readonly string $name_en,
+    ) {
+    }
+}
